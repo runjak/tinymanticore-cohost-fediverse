92478c62e9c375b1ea33f4b91d7f7d7dfbfc275e
Jakob Runge <github@runjak.codes>
2022-11-16 12:08:58 +0100

    upload first image to mastodon using masto
    - replacing the megalodon library with masto
    - oauth is likely broken now

--- test-publish-image.mjs
+++ test-publish-image.mjs
@@ -1,29 +1,44 @@
-import { Mastodon } from "megalodon";
+import { createReadStream } from "fs";
+import { join } from "path";
+import { writeFile, mkdtemp, rm, rmdir } from "fs/promises";
+import { login } from "masto";
 import { config } from "./config.mjs";
-import { writeFile, readFile } from "fs/promises";
 
-const client = new Mastodon(
-  config.mastodon.baseUrl,
-  config.mastodon.accessToken
-);
+const clientPromise = login({
+  url: config.mastodon.baseUrl,
+  accessToken: config.mastodon.accessToken,
+});
 
 const imageUrl =
   "https://www.runjak.codes/_next/image?url=%2Fimages%2Fprofile.jpg&w=256&q=75";
-const imageDescription = "Lurch lurch!";
+const imageDescription = "Aha, der selbe Post, nochmal!";
 
 (async () => {
   console.log("fetching image");
   const imageResponse = await fetch(imageUrl);
-  const imageData = Buffer.from(await imageResponse.arrayBuffer());
+
+  const tempDir = await mkdtemp("/tmp/");
+  const tempFile = join(tempDir, "foo");
+  await writeFile(tempFile, imageResponse.body);
 
   console.log("uploading image");
-  const mediaResponse = await client.uploadMedia(new Blob([imageData]), {
+
+  const client = await clientPromise;
+
+  const attachment = await client.mediaAttachments.create({
+    file: createReadStream(tempFile),
     description: imageDescription,
   });
-  console.log(mediaResponse.data);
+  console.log(attachment);
 
   console.log("posting status");
-  await client.postStatus("test post", { media_ids: [mediaResponse.data.id] });
+  const status = await client.statuses.create({
+    mediaIds: [attachment.id],
+    status: "Oh nice, mach den selben Post nochmal!!",
+  });
+  console.log(status);
 
+  await rm(tempFile);
+  await rmdir(tempDir);
   console.log("it fiiiine, we're done, the war is over.");
 })();
